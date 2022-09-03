51642125b99138e0d920ecfbbb938c5315523dd2
melohugo4 <hugoq2004@gmail.com>
2022-09-03 18:30:34 -0300

    commit c

--- teste.c
+++ teste.c
@@ -1,7 +1,37 @@
-#include<stdio.h>
+#include<unistd.h>
 
-int main()
+void ft_printchar(char p)
 {
-    printf("hello word");
-    return 0;
+    write(1, &p, 1);
+}
+
+void ft_print_comb()
+{
+    int ascii, ascii2, ascii3;
+
+    ascii = 48;
+
+    int i;
+
+    i = 0;
+
+    while (i <= 7)
+    {
+        ascii = ascii + i;
+        ascii2 = ascii;
+        while (ascii2 <= 57)
+        {
+            ascii2 = ascii2 + 1;
+            ascii3 = ascii2 + 1;
+            while (ascii3 <= 57)
+            {
+                ft_printchar(ascii);
+                ft_printchar(ascii2);
+                ft_printchar(ascii3);
+            } 
+        }
+
+        i++;
+    }
+    
 }
